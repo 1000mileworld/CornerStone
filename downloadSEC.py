3cea83285ec6595da3e598346304cb82d85addd1
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-07-27 12:09:06 -0400

    problem with filing json link

--- downloadSEC.py
+++ downloadSEC.py
@@ -24,7 +24,7 @@ for ticker in Symbols:
               'output':'',
               'count':'100'}
 
-        print(f"Getting URL for {ticker}")
+        print(f"Getting search page results URL for {ticker}")
         response = requests.get(url = endpoint, params = param_dict)
         count = 1
         while response.status_code!=200: # keep trying until server says ok
@@ -44,7 +44,7 @@ for ticker in Symbols:
         doc_table = soup.find_all('table', class_='tableFile2')
         master_list = []
         # loop through each row in the table.
-        for row in doc_table[0].find_all('tr')[0:3]: #first 4 rows including table header
+        for row in doc_table[0].find_all('tr')[0:2]: #first few rows including table header
             
             # find all the columns
             cols = row.find_all('td')
@@ -55,7 +55,7 @@ for ticker in Symbols:
                 # grab the text
                 filing_type = cols[0].text.strip()                 
                 filing_date = cols[3].text.strip()
-                filing_numb = cols[4].text.strip()
+                filing_num = cols[4].text.strip()
                 
                 # find the links
                 filing_doc_href = cols[1].find('a', {'href':True, 'id':'documentsbutton'})       
@@ -83,7 +83,7 @@ for ticker in Symbols:
                 # create and store data in the dictionary
                 file_dict = {}
                 file_dict['file_type'] = filing_type
-                file_dict['file_number'] = filing_numb
+                file_dict['file_number'] = filing_num
                 file_dict['file_date'] = filing_date
                 file_dict['links'] = {}
                 file_dict['links']['documents'] = filing_doc_link
@@ -94,10 +94,33 @@ for ticker in Symbols:
                 print('-'*100)        
                 print("Filing Type: " + filing_type)
                 print("Filing Date: " + filing_date)
-                print("Filing Number: " + filing_numb)
+                print("Filing Number: " + filing_num)
                 print("Document Link: " + filing_doc_link)
                 print("Filing Number Link: " + filing_num_link)
                 print("Interactive Data Link: " + filing_int_link)
                 
                 # append dictionary to master list
                 master_list.append(file_dict)
+        
+        #----------Get complete submission text file--------------
+        print('-'*100)  
+        print(f"Getting specific document URL for {ticker}")
+
+        doc_link = master_list[0]['links']['documents'] #filing detail page URL
+        content = utilities.get_url(doc_link,'content')
+        soup = BeautifulSoup(content, 'html.parser')
+        doc_table2 = soup.find_all('table', class_='tableFile')
+        file_row = doc_table2[0].find_all('tr')[-1]
+        cols = file_row.find_all('td')
+        file_href = cols[2].find('a')
+        file_link = base_url_sec + file_href['href']
+        print(file_link)
+
+        print(f"Getting Filing Summary.xml URL for {ticker}")
+        documents_url = file_link.replace('-','').replace('.txt','/index.json')
+        content = utilities.get_url(documents_url)
+        for file in content['directory']['item']:
+            # Grab the filing summary and create a new url leading to the file so we can download it.
+            if file['name'] == 'FilingSummary.xml':
+                xml_summary = base_url_sec + content['directory']['name'] + "/" + file['name']
+                print(xml_summary)
