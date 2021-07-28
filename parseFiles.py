a86bbd6b5487120da9ae12b528706960e32d271d
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-07-28 13:56:23 -0400

    search files in folder

--- parseFiles.py
+++ parseFiles.py
@@ -1,11 +1,18 @@
 # Parse files from Filing Summary URL
+from os import listdir
+from os.path import isfile, join
 
 year = 2015
-Symbols = ['AAPL']
 
-for ticker in Symbols:
+mypath = f"Data\Prices\{year}\\"
+Symbols = [f.split('.')[0] for f in listdir(mypath) if isfile(join(mypath, f))]
+print(Symbols[0:3])
 
-    with open(f"Data\Filing Summaries\{year}\\"+ticker+"_url.txt") as f:
-        summary_url = f.read()
+# Symbols = ['AAPL']
 
-    print(summary_url)
+# for ticker in Symbols:
+
+#     with open(f"Data\Filing Summaries\{year}\\"+ticker+"_url.txt") as f:
+#         summary_url = f.read()
+
+#     print(summary_url)
