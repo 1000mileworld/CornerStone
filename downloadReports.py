d0bce29eb00e8bc8f56db6c5a4ed09cd8d04aff0
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-08-01 11:59:05 -0400

    Add files via upload

--- downloadReports.py
+++ downloadReports.py
@@ -1,28 +1,24 @@
-from utilities import load_obj
+# download specific reports from SEC
+
+from json import load
+
+from requests.api import get
+from utilities import load_obj, get_url
 
 year = 2015
 save_path = f"Data\Reports\{year}\\"
-ticker_info = load_obj('ticker_info')
 
-report_list = [
-    r"Consolidated Balance Sheets",
-    r"Consolidated Statements of Operations and Comprehensive Income (Loss)",
-    r"Consolidated Statements of Cash Flows",
-    r"Consolidated Statements of Stockholder's (Deficit) Equity"
-    ]
-for i in range(len(report_list)):
-    report_list[i] = report_list[i].upper()
+report_urls = load_obj("report_urls")
+Symbols = list(report_urls.keys())
 
-Symbols = []
-for item in list(ticker_info.items()):
-    Symbols.append(item[0])
-
-statements_url = {}
-Symbols = Symbols[:3]
-for i, ticker in enumerate(Symbols):
+#print(Symbols)
+Symbols = ['AAPL']
+for i,ticker in enumerate(Symbols):
     print('-'*100)
-    print(f'Getting reports for {i+1} of {len(Symbols)} tickers ({ticker}): '+ticker_info[ticker]['base_url'])
-    for report_dict in ticker_info[ticker]['reports']:
-        report = report_dict['name_short'].upper()
-        if report in report_list:
-            print('Downloading '+report_dict['name_short']+': '+report_dict['url'])
+    print(f'Downloading reports for {i+1} of {len(Symbols)} tickers ({ticker})...')
+    for type in report_urls[ticker]:
+        print(f'Downloading {type.upper()} report for {ticker}: '+report_urls[ticker][type])
+        content = get_url(report_urls[ticker][type],'content')
+        f = open(save_path+ticker+f'_{type}'+".html", "wb")
+        f.write(content)
+        f.close()
