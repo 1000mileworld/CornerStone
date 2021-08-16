7a829d45177830c0bcaae9a1bace786b15f58fe2
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-08-15 23:36:44 -0400

    Add files via upload

--- Examples/getStatements.py
+++ Examples/getStatements.py
@@ -8,17 +8,27 @@ load_path = f"..\Data\Prices\{year}\\"
 save_path = f"Data\\"
 
 Symbols = [f.split('.')[0] for f in listdir(load_path) if isfile(join(load_path, f))]
+Symbols = Symbols[50:]
+#Symbols = ["ACEVU"]
 
 for i,ticker in enumerate(Symbols):
     print(f'Getting data for {i+1} of {len(Symbols)} ({ticker}):')
     fd = finpie.Fundamentals(ticker, source = 'macrotrends', freq = 'A')
     
     print("Downloading cashflow statement...")
-    df1 = fd.cashflow_statement()
-    df1.to_csv(save_path+f'Cashflow\{ticker}_cashflow.csv')
+    try:
+        df1 = fd.cashflow_statement()
+        df1.to_csv(save_path+f'Cashflow\{ticker}_cashflow.csv')
+    except:
+        print("No data found!")
+        continue
 
     print("Downloading income statement....")
-    df2 = fd.income_statement()
-    df2.to_csv(save_path+f'Income\{ticker}_income.csv')
+    try:
+        df2 = fd.income_statement()
+        df2.to_csv(save_path+f'Income\{ticker}_income.csv')
+    except:
+        print("No data found!")
+        continue
 
 print("Done!")
