d847b829315a1619bf9a9a25e7ba33bdc2062379
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-08-24 11:41:45 -0400

    made into function

--- Examples/getStatements.py
+++ Examples/getStatements.py
@@ -5,22 +5,6 @@ import time
 from datetime import date
 import os
 
-copy = 1
-max_errors = 15 #how many errors in a row before program thinks it's a connection problem
-
-text_path = f"Texts\\"
-save_path = f"Data\\"
-invalid = np.loadtxt(text_path+"No Download.txt",dtype='str')
-pos_file = f"Ticker Position {copy}.txt" #keeps track of the Symbol index for the current ticker whose data is being downloaded
-log_file = f"Log {copy}.txt"
-
-with open(text_path+pos_file,'r') as f:
-    startPos = int(f.readline().strip())
-stopPos = startPos+1000
-
-Symbols = np.loadtxt(text_path+"YF Tickers Filtered.txt",dtype='str')
-Symbols = Symbols[startPos:stopPos]
-
 def getData(ticker,s_type,save_path,copy):
     print(f"Downloading {s_type} statement...")
 
@@ -48,39 +32,70 @@ def connect(host='http://google.com'):
     except:
         return False
 
-valErrors = 0
-for i,ticker in enumerate(Symbols):
-    print(f'Getting data for {i+1} of {len(Symbols)} ({ticker})')
-    with open(text_path+pos_file,'w') as f:
-        f.write(str(startPos+i))
+def writeLog(file,mode,text):
+    with open(file,mode) as f:    
+        f.write(text)
 
-    if not ticker in invalid:
-        result1 = getData(ticker,"cashflow",save_path,copy)
-        if result1==3:
-            valErrors+=1
-            if valErrors>=max_errors:                
-                print("Too many tickers unable to be imported to pandas, checking internet connection...")
-                if not connect():
-                    with open(text_path+pos_file,'w') as f:
-                        f.write(str(startPos+i+1-max_errors)) #max_errors start counting at 1 while startPos is zero indexed
-                    
-                    print("System restarting in 10 seconds...")
-                    today = date.today()
-                    t = time.localtime()
-                    current_time = time.strftime("%H:%M:%S", t)
-                    with open(text_path+log_file,'a') as f:    
-                        f.write(f"Restarted PC at {current_time} local time on {today}\n")
-                    os.system("shutdown /r /t 10")
-                else:
-                    print("Internet connection verified, increasing max errors allowed by 5.")
-                    max_errors+=5
-                #exit()
+def getTimeStr():
+    today = date.today()
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return f"{today} {current_time} local time:"
+
+def download(copy):
+    max_errors = 15 #how many errors in a row before program thinks it's a connection problem
+
+    text_path = f"Texts\\"
+    save_path = f"Data\\"
+    invalid = np.loadtxt(text_path+"No Download.txt",dtype='str')
+    pos_file = f"Ticker Position {copy}.txt" #keeps track of the Symbol index for the current ticker whose data is being downloaded
+    log_file = f"Log {copy}.txt"
+
+    time_str = getTimeStr()
+    if not connect():
+        writeLog(text_path+log_file,'a',f"{time_str} script unable to start due to no internet connection\n")
+        exit()
+    writeLog(text_path+log_file,'a',f"{time_str} script started\n")
+
+    with open(text_path+pos_file,'r') as f:
+        startPos = int(f.readline().strip())
+    stopPos = copy*1000
+
+    Symbols = np.loadtxt(text_path+"YF Tickers Filtered.txt",dtype='str').tolist()
+    Symbols = Symbols[startPos:stopPos]
+
+    valErrors = 0
+    for i,ticker in enumerate(Symbols):
+        print(f'Getting data for {i+1} of {len(Symbols)} ({ticker})')
+        writeLog(text_path+pos_file,'w',str(startPos+i))
+        if not ticker in invalid:
+            result1 = getData(ticker,"cashflow",save_path,copy)
+            if result1==3:
+                valErrors+=1
+                if valErrors>=max_errors:                
+                    print("Too many tickers unable to be imported to pandas, checking internet connection...")
+                    if not connect():
+                        print("No internet connection found, system restarting in 30 seconds...")
+                        writeLog(text_path+pos_file,'w',str(startPos+i+1-max_errors)) #max_errors start counting at 1 while startPos is zero indexed                       
+                        time_str = getTimeStr()
+                        writeLog(text_path+log_file,'a',f"{time_str} PC restarted\n")
+                        os.system("shutdown /r /t 30")
+                    elif not connect(host="https://www.macrotrends.net/"):
+                        print("Internet connection verified, but no connection received from Macrotrends.")
+                        time_str = getTimeStr()
+                        writeLog(text_path+log_file,'a',f"{time_str} script exited due to no connection from Macrotrends\n")
+                        exit()
+                    else:
+                        print("Internet connection verified, increasing max errors allowed by 5.")
+                        max_errors+=5
+            else:
+                valErrors = 0
+
+            if result1==1: #first retrieval succeeded
+                result2 = getData(ticker,"income",save_path,copy)
         else:
-            valErrors = 0
+            print(f"Skipped {ticker} due to it being on the invalid tickers list.")
+        
+    print("Done!")
 
-        if result1==1: #first retrieval succeeded
-            result2 = getData(ticker,"income",save_path,copy)
-    else:
-        print(f"Skipped {ticker} due to it being on the invalid tickers list.")
-    
-print("Done!")
+
