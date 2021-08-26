5718e00c858b8bcab97b23ad0b9162b8ebdd9e3f
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-08-25 21:52:28 -0400

    updated save structure
    updated "No Download.txt" and save as sorted

--- Examples/findMissing.py
+++ Examples/findMissing.py
@@ -10,8 +10,8 @@ origin_path = f"..\Data\Prices\{year}\\"
 statements_path = "Data\\"
 text_path = "Texts\\"
 base_url = "https://www.macrotrends.net/stocks/charts/"
-outfile = text_path+"Missing Symbols.txt"
-invalid = np.loadtxt(text_path+"No Download.txt",dtype='str')
+outfile = text_path+"Missing Symbols.txt" # remember to delete it if file exists
+exceptionsFile = text_path+"No Download.txt" 
 
 s_origin = [f.split('.')[0] for f in listdir(origin_path) if isfile(join(origin_path, f))]
 s_cashflow = [f.split('_')[0] for f in listdir(statements_path+"Cashflow") if isfile(join(statements_path+"Cashflow", f))]
@@ -28,8 +28,6 @@ def saveArray(arr,outfile):
     arr_np = np.array(arr)
     with open(outfile, 'w'):
         np.savetxt(outfile,arr_np,fmt='%s')
-    #loaded = np.loadtxt(outfile,dtype='str')
-    #print(loaded.tolist())
 
 def get_url(url):
     for x in range(0, 5):  # try 5 times
@@ -43,19 +41,26 @@ def get_url(url):
     return response
 
 s_missing = compare(s_origin,s_income,outfile)
-s_missing = s_missing[563:]
-has_url = []
-for i,ticker in enumerate(s_missing):
-    print(f'Checking ticker {i+1} of {len(s_missing)} ({ticker})...')
-    if not ticker in invalid:
+exceptions = np.loadtxt(exceptionsFile,dtype='str').tolist()
+
+s_missing2 = []
+for ticker in s_missing:
+    if not ticker in exceptions:
+        s_missing2.append(ticker)
+
+#s_missing2 = s_missing2[563:]
+for i,ticker in enumerate(s_missing2):
+    print(f'Checking ticker {i+1} of {len(s_missing2)} ({ticker})...')
+    if not ticker in exceptions:
         url = base_url+ticker
         response = get_url(url)
         if response.history:
             # Request was redirected, status code 301
             with open(outfile,'a+') as f:
                 f.write(f"{ticker}\n")
-            has_url.append(ticker)
             #print(response.url)
-
-print(has_url)
-#saveArray(has_url,outfile)
+        else:
+            with open(exceptionsFile,'a') as f:
+                f.write(f"{ticker}\n")
+exceptions.sort()
+saveArray(exceptions,exceptionsFile)
