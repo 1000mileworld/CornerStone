127662f9ededd5f4402aef507353ab226ef3663e
1000mileworld <56658669+1000mileworld@users.noreply.github.com>
2021-09-01 20:48:04 -0400

    added val stock selection

--- MATLAB/cornerstone.m
+++ MATLAB/cornerstone.m
@@ -33,41 +33,67 @@ avg_cash = sum(T.CashFlow)/totalStocks;
 avg_sales = sum(T.Revenue)/totalStocks; %in millions
 
 %% Find growth stock candidates
-T_growth = T;
+% T_growth = T;
+% 
+% %Desired criteria
+% condition = T_growth.MarketCap>300e6; %small cap
+% T_growth(~condition,:) = [];
+% 
+% condition = T_growth.MarketCap./(T_growth.Revenue.*1e6)<1.5 & T_growth.Revenue>0; %P/S ratio
+% T_growth(~condition,:) = [];
+% 
+% condition = T_growth.NetIncome>T_growth.NetIncomePrev & T_growth.NetIncome>0 & T_growth.NetIncomePrev>0; %Earnings higher than previous year
+% T_growth(~condition,:) = [];
+% 
+% condition = T_growth.ThreeMonthPriceAppreciation>avg_3M; %3 month price appreciation greater than avg.
+% T_growth(~condition,:) = [];
+% 
+% condition = T_growth.SixMonthPriceAppreciation>avg_6M; %6 month price appreciation greater than avg.
+% T_growth(~condition,:) = [];
+% 
+% T_growth.ProfitMargin = T_growth.NetIncome./T_growth.Revenue;
+% ind = find(strcmpi(T_growth.Properties.VariableNames,'ProfitMargin')); %rank top 100 by profit margin
+% T_growth = sortrows(T_growth,ind,'descend');
+% if height(T_growth)>100
+%    T_growth(101:end,:) = []; 
+% end
+% 
+% ind = find(strcmpi(T_growth.Properties.VariableNames,'TwelveMonthPriceAppreciation'));
+% T_growth = sortrows(T_growth,ind,'descend');
+% 
+% if height(T_growth)<numStocks
+%     growthStocks = T_growth.Ticker;
+% else
+%     growthStocks = T_growth.Ticker(1:numStocks);
+% end
+% 
+% writeData(growthStocks,"growth.txt")
 
-%Desired criteria
-condition = T_growth.MarketCap>300e6; %small cap
-T_growth(~condition,:) = [];
+%% Find value stock candidates
+T_value = T;
 
-condition = T_growth.MarketCap./(T_growth.Revenue.*1e6)<1.5 & T_growth.Revenue>0; %P/S ratio
-T_growth(~condition,:) = [];
+%get market leaders
+condition = T_value.MarketCap>=10e9; %large cap
+T_value(~condition,:) = [];
 
-condition = T_growth.NetIncome>T_growth.NetIncomePrev & T_growth.NetIncome>0 & T_growth.NetIncomePrev>0; %Earnings higher than previous year
-T_growth(~condition,:) = [];
+condition = T_value.Sector~="Public Utilities"; %Nonutility stock
+T_value(~condition,:) = [];
 
-condition = T_growth.ThreeMonthPriceAppreciation>avg_3M; %3 month price appreciation greater than avg.
-T_growth(~condition,:) = [];
+factor = 1.5; %greater than database avg by factor in following areas
+condition = T_value.MarketCap>=avg_MCap*factor & T_value.SharesOutstanding>=avg_shares*factor...
+    & T_value.CashFlow>=avg_cash*factor & T_value.Revenue>avg_sales*factor;
+T_value(~condition,:) = [];
 
-condition = T_growth.SixMonthPriceAppreciation>avg_6M; %6 month price appreciation greater than avg.
-T_growth(~condition,:) = [];
+ind = find(strcmpi(T_value.Properties.VariableNames,'ShareholderYield'));
+T_value = sortrows(T_value,ind,'descend'); %rank by shareholder yield
 
-T_growth.ProfitMargin = T_growth.NetIncome./T_growth.Revenue;
-ind = find(strcmpi(T_growth.Properties.VariableNames,'ProfitMargin')); %rank top 100 by profit margin
-T_growth = sortrows(T_growth,ind,'descend');
-if height(T_growth)>100
-   T_growth(101:end,:) = []; 
-end
-
-ind = find(strcmpi(T_growth.Properties.VariableNames,'TwelveMonthPriceAppreciation'));
-T_growth = sortrows(T_growth,ind,'descend');
-
-if height(T_growth)<numStocks
-    growthStocks = T_growth.Ticker;
+if height(T_value)<numStocks
+    valStocks = T_value.Ticker;
 else
-    growthStocks = T_growth.Ticker(1:numStocks);
+    valStocks = T_value.Ticker(1:numStocks);
 end
 
-writeData(growthStocks,"growth.txt")
+writeData(valStocks,"value.txt")
 
 %% Functions
 function [] = writeData(data,fileName)
